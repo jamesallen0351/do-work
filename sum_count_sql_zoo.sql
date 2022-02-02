a7a913f55170f50ac8716903350594539245ac90
jamesallen0351 <james.allen0351@gmail.com>
2022-02-02 07:42:05 -0600

    add sum_count problem 1 in progress

---
+++ sum_count_sql_zoo.sql
@@ -0,0 +1,12 @@
+/* SQL Zoo SUM and COUNT Exercises */
+
+/* World Country Profile: Aggregate functions
+This tutorial is about aggregate functions such as COUNT, SUM and AVG. An aggregate function takes many values 
+and delivers just one value. 
+For example the function SUM would aggregate the values 2, 4 and 5 to deliver the single value 11. */
+
+/* 1. Show the total population of the world. */
+
+SELECT SUM(population)
+FROM world
+
