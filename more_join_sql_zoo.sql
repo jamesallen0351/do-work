dda8c39f32c4e02285696bb60ea74f5e5d949802
jamesallen0351 <james.allen0351@gmail.com>
2022-02-08 07:18:28 -0600

    add problems 7 to more join

--- more_join_sql_zoo.sql
+++ more_join_sql_zoo.sql
@@ -4,3 +4,41 @@
 
 SELECT id, title FROM movie
 WHERE yr = 1962;
+
+/* 2. Give year of 'Citizen Kane' */
+
+SELECT yr FROM movie
+WHERE title = 'Citizen Kane'
+
+/* 3. List all of the Star Trek movies, include id, title, and yr 
+    (all of these movies include the words Star Trek in the title)
+    Order results by year. */
+
+SELECT id, title, yr FROM movie
+WHERE title LIKE 'Star Trek%'
+ORDER BY yr
+
+/* 4. What id number does the actor 'Glenn Close' have? */
+
+SELECT id FROM actor
+WHERE name = 'Glenn Close'
+
+/* 5. What is the id of the film 'Casablanca' */
+
+SELECT id FROM movie
+WHERE title = 'Casablanca'
+
+/* 6. Obtain the cast list for 'Casablanca'.
+    Use movieid=11768 */
+
+SELECT name FROM actor
+JOIN casting ON id = actorid
+WHERE movieid = 11768
+
+/* 7. Obtain the cast list for the film 'Alien' */
+    -- movieid = 10522 --
+
+SELECT name FROM actor
+JOIN casting ON id = actorid
+WHERE movieid = 10522
+
