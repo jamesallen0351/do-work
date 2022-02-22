4fe9fb6af2bd6f373d094f289fdeafc33719362e
jamesallen0351 <james.allen0351@gmail.com>
2022-02-22 07:19:00 -0600

    add self join problem 3

--- self_join.sql
+++ self_join.sql
@@ -9,3 +9,9 @@ SELECT COUNT(id) FROM stops
 SELECT id FROM stops
 WHERE name = 'Craiglockhart'
 
+/* 3. Give the id and the name for the stops on the '4' 'LRT' service. */ 
+
+SELECT DISTINCT id, stops.name FROM route
+JOIN stops ON stop = stops.id
+WHERE num = 4 AND company = 'LRT'
+
