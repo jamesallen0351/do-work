f82da4c1ebf215331d23285e05e59732a7340f27
jamesallen0351 <james.allen0351@gmail.com>
2022-03-05 08:12:24 -0600

    add hackerrank advanced join symmetric pairs

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -57,3 +57,10 @@ GROUP BY SUBMISSION_DATE;
 /* Write a query to output all such symmetric pairs in ascending order by the value of X. 
 List the rows such that X1 ≤ Y1. */
 
+SELECT f1.X, f1.Y FROM Functions AS f1 
+WHERE f1.X = f1.Y AND
+(SELECT COUNT(*) FROM Functions WHERE X = f1.X AND Y = f1.Y) > 1
+UNION
+SELECT f1.X, f1.Y from Functions AS f1
+WHERE EXISTS(SELECT X, Y FROM Functions WHERE f1.X = Y AND f1.Y = X AND f1.X < X)
+ORDER BY X;
