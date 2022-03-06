71df165484a9a00288aa1ea8cb52259a1eb4a827
jamesallen0351 <james.allen0351@gmail.com>
2022-03-06 08:02:40 -0600

    add hackerrank weather observation station 2

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -64,3 +64,9 @@ UNION
 SELECT f1.X, f1.Y from Functions AS f1
 WHERE EXISTS(SELECT X, Y FROM Functions WHERE f1.X = Y AND f1.Y = X AND f1.X < X)
 ORDER BY X;
+
+/* Query the following two values from the STATION table:
+    1. The sum of all values in LAT_N rounded to a scale of  decimal places.
+    2. The sum of all values in LONG_W rounded to a scale of  decimal places. */
+
+SELECT ROUND(SUM(LAT_N), 2) AS lat, ROUND(SUM(LONG_W), 2) AS lon FROM STATION;
