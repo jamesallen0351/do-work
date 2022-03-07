a2bf545290c2c5ed3c39aaa7d3f03f7b2c844034
jamesallen0351 <james.allen0351@gmail.com>
2022-03-07 07:12:52 -0600

    add hackerrank weather observation station 13

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -70,3 +70,8 @@ ORDER BY X;
     2. The sum of all values in LONG_W rounded to a scale of  decimal places. */
 
 SELECT ROUND(SUM(LAT_N), 2) AS lat, ROUND(SUM(LONG_W), 2) AS lon FROM STATION;
+
+/* Query the sum of Northern Latitudes (LAT_N) from STATION having values 
+    greater than 38.7880 and less than 137.2345. Truncate your answer to  decimal places. */
+
+SELECT TRUNCATE(SUM(LAT_N), 4) FROM STATION WHERE LAT_N BETWEEN 38.7880 AND 137.2345;
