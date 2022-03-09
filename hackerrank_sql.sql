15263eca90d8c047bb2ca0d99678afcd1d3cd706
jamesallen0351 <james.allen0351@gmail.com>
2022-03-09 07:34:03 -0600

    add hackerrank weather observation 15

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -80,3 +80,10 @@ SELECT TRUNCATE(SUM(LAT_N), 4) FROM STATION WHERE LAT_N BETWEEN 38.7880 AND 137.
     Truncate your answer to 4 decimal places */
 
 SELECT TRUNCATE(MAX(LAT_N),4) FROM STATION WHERE LAT_N < 137.2345;
+
+/* Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION
+    that is less than 137.2345. Round your answer to 4 decimal places. */
+
+SELECT ROUND(LONG_W, 4) FROM STATION
+WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION
+WHERE LAT_N < 137.2345);
