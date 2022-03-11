6089fb0936915220ecf40b3ebb69f99e770bce1b
jamesallen0351 <james.allen0351@gmail.com>
2022-03-11 07:51:38 -0600

    add hackerrank weather station 17

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -93,3 +93,9 @@ WHERE LAT_N < 137.2345);
 
 SELECT ROUND(MIN(LAT_N),4) FROM STATION
 WHERE LAT_N > 38.7780;
+
+/* Query the Western Longitude (LONG_W) where the smallest Northern Latitude (LAT_N) in 
+    STATION is greater than 38.7780. Round your answer to 4 decimal places. */
+
+SELECT ROUND(LONG_W, 4) FROM STATION 
+WHERE LAT_N = (SELECT MIN(LAT_N) FROM STATION WHERE LAT_N > 38.7780);
