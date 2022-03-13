a20ed1b4225b13b1576a1bc4e7a327e0578e14f0
jamesallen0351 <james.allen0351@gmail.com>
2022-03-13 07:16:47 -0500

    add hackerrank advanced joins placements

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -109,3 +109,11 @@ separated integers */
 
 SELECT MAX(salary * months), COUNT(salary * months) FROM Employee
 WHERE (salary * months) = (SELECT MAX(salary * months) FROM Employee);
+
+/* Write a query to output the names of those students whose best friends got offered a higher
+ salary than them. Names must be ordered by the salary amount offered to the best friends. 
+ It is guaranteed that no two students got same salary offer. */
+
+select temp1.sn from 
+(select S.ID si,S.Name sn,P.Salary ps from Students S join Packages P on S.ID=P.ID) temp1 join (select FF.ID fi,FF.Friend_ID fd,PP.Salary pps from Friends FF join Packages PP on FF.Friend_ID=pp.ID) temp2 on temp1.si=temp2.fi and temp1.ps<temp2.pps
+order by temp2.pps asc;
