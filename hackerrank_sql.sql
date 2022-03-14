eab4151a37f735afc1ceb1553c6283d279943525
jamesallen0351 <james.allen0351@gmail.com>
2022-03-14 07:12:32 -0500

    add hackrrank advanced join

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -117,3 +117,16 @@ WHERE (salary * months) = (SELECT MAX(salary * months) FROM Employee);
 select temp1.sn from 
 (select S.ID si,S.Name sn,P.Salary ps from Students S join Packages P on S.ID=P.ID) temp1 join (select FF.ID fi,FF.Friend_ID fd,PP.Salary pps from Friends FF join Packages PP on FF.Friend_ID=pp.ID) temp2 on temp1.si=temp2.fi and temp1.ps<temp2.pps
 order by temp2.pps asc;
+
+/* Write a query to output the start and end dates of projects listed by the number of days
+ it took to complete the project in ascending order. If there is more than one project that 
+ have the same number of completion days, then order by the start date of the project. */
+
+SELECT Start_Date, min(End_Date)
+FROM 
+ (SELECT Start_Date FROM Projects WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)) a ,
+ (SELECT End_Date FROM Projects WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)) b
+WHERE Start_Date < End_Date
+GROUP BY Start_Date
+ORDER BY DATEDIFF(min(End_Date), Start_Date) ASC, Start_Date ASC;
+
