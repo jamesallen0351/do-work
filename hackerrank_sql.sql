231f89abe8f99e00214ed768e3db1e75aa9143e8
jamesallen0351 <james.allen0351@gmail.com>
2022-03-16 07:53:21 -0500

    add hackerrank draw the triangle 1

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -134,3 +134,11 @@ ORDER BY DATEDIFF(min(End_Date), Start_Date) ASC, Start_Date ASC;
 
 SELECT COUNT(*) FROM CITY
 WHERE POPULATION > 100000;
+
+/* P(R) represents a pattern drawn by Julia in R rows.
+    Write a query to print the pattern P(20). */
+
+SET @no_of_lines = 20 + 1;
+
+SELECT REPEAT('* ', @no_of_lines := @no_of_lines -1) 
+FROM INFORMATION_SCHEMA.TABLES;
