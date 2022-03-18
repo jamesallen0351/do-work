ff587b46cf3f0128617f61f0c29b93b31a6dc56a
jamesallen0351 <james.allen0351@gmail.com>
2022-03-18 07:36:09 -0500

    add hackerrank print prime numbers

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -148,3 +148,24 @@ FROM INFORMATION_SCHEMA.TABLES;
 
 set @row := 0;
 select repeat('* ', @row := @row + 1) from information_schema.tables where @row < 20;
+
+/* Write a query to print all prime numbers less than or equal to 1000. Print your results on
+a single line, and use the ampersand (&) character as your separator. */
+
+SELECT GROUP_CONCAT(NUMB SEPARATOR '&')
+FROM (
+    SELECT @num:=@num+1 as NUMB FROM
+    information_schema.tables t1,
+    information_schema.tables t2,
+    (SELECT @num:=1) tmp
+) tempNum
+WHERE NUMB<=1000 AND NOT EXISTS(
+		SELECT * FROM (
+			SELECT @nu:=@nu+1 as NUMA FROM
+			    information_schema.tables t1,
+			    information_schema.tables t2,
+			    (SELECT @nu:=1) tmp1
+			    LIMIT 1000
+			) tatata
+		WHERE FLOOR(NUMB/NUMA)=(NUMB/NUMA) AND NUMA<NUMB AND NUMA>1
+	)
