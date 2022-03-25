b7000262ef1413e542a5043c2dd838be9a668e37
jamesallen0351 <james.allen0351@gmail.com>
2022-03-25 07:43:03 -0500

    add data camp exercises

--- data_camp.py
+++ data_camp.py
@@ -5,3 +5,19 @@ desc = "compound interest"
 
 # Create a variable profitable
 profitable = True
+
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+
+# Assign product of savings and growth_multiplier to year1
+year1 = savings * growth_multiplier
+
+# Print the type of year1
+type(year1)
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = sum(desc) + desc
+
+# Print out doubledesc
+print(doubledesc)
