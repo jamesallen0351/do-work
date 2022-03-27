654972e127bee92a35a793dfa1b355d1b7e2f578
jamesallen0351 <james.allen0351@gmail.com>
2022-03-27 07:41:21 -0500

    add data camp string and float exercise

--- data_camp.py
+++ data_camp.py
@@ -37,3 +37,29 @@ doubledesc = desc + desc
 
 # Print out doubledesc
 print(doubledesc)
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + savings + " and now have $" + result + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+
+# Solution
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
