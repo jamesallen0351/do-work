cf50b6f6566f953802401edd4790af5dd8fe51c7
jamesallen0351 <james.allen0351@gmail.com>
2022-04-01 06:44:58 -0500

    add list elements solutions

--- data_camp.py
+++ data_camp.py
@@ -116,10 +116,10 @@ print(type(house))
 areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
 
 # Print out second element from areas
-print(areas[_])
+print(areas[1])
 
 # Print out last element from areas
-print(areas[__])
+print(areas[-1])
 
 # Print out the area of the living room
-print(areas[_])
+print(areas[5])
