d44c24909d26dd135c69f18f347437057b192476
jamesallen0351 <james.allen0351@gmail.com>
2022-04-03 07:17:58 -0500

    add slicing in progress

--- data_camp.py
+++ data_camp.py
@@ -132,3 +132,14 @@ eat_sleep_area = areas[3] + areas[-3]
 
 # Print the variable eat_sleep_area
 print(eat_sleep_area)
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Use slicing to create downstairs
+
+
+# Use slicing to create upstairs
+
+
+# Print out downstairs and upstairs
