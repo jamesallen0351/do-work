324e0d04d9b398cfa7c41b64bd020085b0c682f0
jamesallen0351 <james.allen0351@gmail.com>
2022-04-10 08:16:11 -0500

    add multiple arguments in progress

--- data_camp.py
+++ data_camp.py
@@ -198,3 +198,15 @@ print(len(var1))
 
 # Convert var2 to an integer: out2
 out2 = int(var2)
+
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+
+
+# Sort full in descending order: full_sorted
+
+
+# Print out full_sorted
